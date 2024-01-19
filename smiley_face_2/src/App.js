fa87424263b9f2893379f349ab7092b7d4558280
sean <spmcdonald1316@gmail.com>
2024-01-19 16:06:50 -0600

    add eyes to face using variables and JSX to calculate positioning

--- smiley_face_2/src/App.js
+++ smiley_face_2/src/App.js
@@ -5,6 +5,9 @@ const height = 500;
 const centerX = width / 2;
 const centerY = height / 2;
 const strokeWidth = 10;
+const eyeOffsetX = 90;
+const eyeOffsetY = 100;
+const eyeRadius = 40;
 
 function App() {
   return (
@@ -12,11 +15,21 @@ function App() {
       <circle
         cx={centerX}
         cy={centerY}
-        r="245"
+        r={centerY - strokeWidth / 2}
         fill="yellow"
         stroke="black"
         stroke-width={strokeWidth}
-      ></circle>
+      />
+      <circle
+        cx={centerX - eyeOffsetX}
+        cy={centerY - eyeOffsetY}
+        r={eyeRadius}
+      />
+      <circle
+        cx={centerX + eyeOffsetX}
+        cy={centerY - eyeOffsetY}
+        r={eyeRadius}
+      />
     </svg>
   );
 }
