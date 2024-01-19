93349a6698c280a0b71fe44058ee23e0a47b9823
sean <spmcdonald1316@gmail.com>
2024-01-19 15:58:22 -0600

    create js variables and use JSX to add them to smiley face measurements

--- smiley_face_2/src/App.js
+++ smiley_face_2/src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 
+const width = 960;
+const height = 500;
+const centerX = width / 2;
+const centerY = height / 2;
+const strokeWidth = 10;
+
 function App() {
   return (
-    <svg width="960" height="500">
+    <svg width={width} height={height}>
       <circle
-        cx="480"
-        cy="250"
+        cx={centerX}
+        cy={centerY}
         r="245"
         fill="yellow"
         stroke="black"
-        stroke-width="10"
+        stroke-width={strokeWidth}
       ></circle>
     </svg>
   );
