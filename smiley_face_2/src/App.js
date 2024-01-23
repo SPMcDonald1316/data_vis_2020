960f5c6c3ca2f41bfcbf68621543c6becc2fc323
sean <spmcdonald1316@gmail.com>
2024-01-23 15:50:45 -0600

    import d3, use arc function to make mouth for face

--- smiley_face_2/src/App.js
+++ smiley_face_2/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { arc } from 'd3';
 
 const width = 960;
 const height = 500;
@@ -8,28 +9,37 @@ const strokeWidth = 10;
 const eyeOffsetX = 90;
 const eyeOffsetY = 100;
 const eyeRadius = 40;
+const mouthWidth = 20;
+const mouthRadius = 140;
+
+const mouthArc = arc()
+  .innerRadius(mouthRadius)
+  .outerRadius(mouthRadius + mouthWidth)
+  .startAngle(Math.PI / 2)
+  .endAngle(Math.PI * 3 / 2);
 
 function App() {
   return (
     <svg width={width} height={height}>
-      <circle
-        cx={centerX}
-        cy={centerY}
-        r={centerY - strokeWidth / 2}
-        fill="yellow"
-        stroke="black"
-        stroke-width={strokeWidth}
-      />
-      <circle
-        cx={centerX - eyeOffsetX}
-        cy={centerY - eyeOffsetY}
-        r={eyeRadius}
-      />
-      <circle
-        cx={centerX + eyeOffsetX}
-        cy={centerY - eyeOffsetY}
-        r={eyeRadius}
-      />
+      <g transform={`translate(${centerX}, ${centerY})`}>
+        <circle
+          r={centerY - strokeWidth / 2}
+          fill="yellow"
+          stroke="black"
+          stroke-width={strokeWidth}
+        />
+        <circle
+          cx={-eyeOffsetX}
+          cy={-eyeOffsetY}
+          r={eyeRadius}
+        />
+        <circle
+          cx={eyeOffsetX}
+          cy={-eyeOffsetY}
+          r={eyeRadius}
+        />
+        <path d={mouthArc()} />
+      </g>
     </svg>
   );
 }
