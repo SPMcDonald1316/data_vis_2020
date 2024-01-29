23ec5fe89b14afc0748fbb3a18042a9d81bd777c
sean <spmcdonald1316@gmail.com>
2024-01-29 16:52:38 -0600

    set event listener, write handler function to display x, y coords from mouse event

--- mouse_interaction/src/App.jsx
+++ mouse_interaction/src/App.jsx
@@ -6,8 +6,13 @@ const circleX = width / 2;
 const circleY = height / 2;
 const circleRadius = 30;
 
+const handleMouseMove = (event) => {
+  const { clientX, clientY } = event;
+  console.log({clientX, clientY});
+};
+
 const App = () => (
-  <svg width={width} height={height}>
+  <svg width={width} height={height} onMouseMove={handleMouseMove}>
     <circle
       cx={circleX}
       cy={circleY}
