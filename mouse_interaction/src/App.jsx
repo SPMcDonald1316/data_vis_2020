53d293214f10d9756420fdec5d7bf7882cd05952
sean <spmcdonald1316@gmail.com>
2024-01-29 17:06:21 -0600

    use state to keep track of position of mouse and set circle to location

--- mouse_interaction/src/App.jsx
+++ mouse_interaction/src/App.jsx
@@ -1,24 +1,26 @@
 import './App.css'
+import { useState } from 'react';
 
 const width = 960;
 const height = 500;
-const circleX = width / 2;
-const circleY = height / 2;
 const circleRadius = 30;
+const initialMousePostion = { x: width / 2, y: height / 2};
 
-const handleMouseMove = (event) => {
-  const { clientX, clientY } = event;
-  console.log({clientX, clientY});
-};
 
-const App = () => (
-  <svg width={width} height={height} onMouseMove={handleMouseMove}>
-    <circle
-      cx={circleX}
-      cy={circleY}
-      r={circleRadius}
-    ></circle>
-  </svg>
-);
+const App = () => {
+  const [mousePosition, setMousePosition] = useState(initialMousePostion);
+  const handleMouseMove = (event) => {
+    const { clientX, clientY } = event;
+    setMousePosition({x: clientX, y: clientY});
+  };
+  return (
+    <svg width={width} height={height} onMouseMove={handleMouseMove}>
+      <circle
+        cx={mousePosition.x}
+        cy={mousePosition.y}
+        r={circleRadius}
+      ></circle>
+    </svg>
+)};
 
 export default App
