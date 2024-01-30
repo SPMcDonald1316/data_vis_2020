9d388709d82556a25030004d3a5b125d98e76e15
sean <spmcdonald1316@gmail.com>
2024-01-30 17:00:23 -0600

    use d3.csv to fetch and parse data, create message

--- loading_data_into_react/src/App.jsx
+++ loading_data_into_react/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import d3 from 'd3';
 
 const csvUrl = "https://gist.githubusercontent.com/SPMcDonald1316/c44c8efd90878b3c4032c3b642669e19/raw/cssNamedColors.csv";
 const width = 960;
@@ -8,9 +9,16 @@ const height = 500;
 function App = () => {
   const [data, setData] = useState(null);
 
+  d3.csv(csvUrl).then(data => {
+    let message = ''
+    message += `${Math.round(d3.csvFormat(data) / 1024)} kB\n`
+    message += `${data.length} rows\n`;
+    message += `${data.columns.length} columns\n`
+  });
+
   return (
     
-  )
-}
+  );
+};
 
 export default App
