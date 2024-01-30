6cabdc057dae38c48232c8f910dabdda0d3915dd
sean <spmcdonald1316@gmail.com>
2024-01-30 17:15:27 -0600

    fetch data, parse with d3, display message if data loads

--- loading_data_into_react/src/App.jsx
+++ loading_data_into_react/src/App.jsx
@@ -1,23 +1,24 @@
 import { useState } from 'react'
 import './App.css'
-import d3 from 'd3';
+import {csv} from 'd3';
 
 const csvUrl = "https://gist.githubusercontent.com/SPMcDonald1316/c44c8efd90878b3c4032c3b642669e19/raw/cssNamedColors.csv";
 const width = 960;
 const height = 500;
 
-function App = () => {
+const App = () => {
   const [data, setData] = useState(null);
 
-  d3.csv(csvUrl).then(data => {
-    let message = ''
-    message += `${Math.round(d3.csvFormat(data) / 1024)} kB\n`
-    message += `${data.length} rows\n`;
-    message += `${data.columns.length} columns\n`
+  csv(csvUrl).then(data => {
+    setData(data);
+    // let message = ''
+    // message += `${Math.round(d3.csvFormat(data) / 1024)} kB\n`
+    // message += `${data.length} rows\n`;
+    // message += `${data.columns.length} columns\n`
   });
 
   return (
-    
+    <div>Data is {data ? 'loaded' : 'loading'}</div>
   );
 };
 
