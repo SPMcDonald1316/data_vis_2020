eea0399c659bd425dea65e4690c8d2c44973886a
sean <spmcdonald1316@gmail.com>
2024-01-30 17:25:47 -0600

    create message to display info about loaded in data

--- loading_data_into_react/src/App.jsx
+++ loading_data_into_react/src/App.jsx
@@ -1,24 +1,29 @@
 import { useState } from 'react'
 import './App.css'
-import {csv} from 'd3';
+import {csv, csvFormat} from 'd3';
 
 const csvUrl = "https://gist.githubusercontent.com/SPMcDonald1316/c44c8efd90878b3c4032c3b642669e19/raw/cssNamedColors.csv";
 const width = 960;
 const height = 500;
+
+const message = data => {
+  let message = '';
+  message += `${Math.round(csvFormat(data).length / 1024)} kB\n`;
+  console.log(Math.round(csvFormat(data)));
+  message += `${data.length} rows\n`;
+  message += `${data.columns.length} columns\n`;
+  return message;
+}
 
 const App = () => {
   const [data, setData] = useState(null);
 
   csv(csvUrl).then(data => {
     setData(data);
-    // let message = ''
-    // message += `${Math.round(d3.csvFormat(data) / 1024)} kB\n`
-    // message += `${data.length} rows\n`;
-    // message += `${data.columns.length} columns\n`
   });
 
   return (
-    <div>Data is {data ? 'loaded' : 'loading'}</div>
+    <div>Data is {data ? message(data) : 'loading'}</div>
   );
 };
 
