5b6a8a9e1d612bbc0019e8995a47ddaa3daf9c9f
sean <spmcdonald1316@gmail.com>
2024-01-31 16:29:29 -0600

    add small styling to make message larger

--- loading_data_into_react/src/App.jsx
+++ loading_data_into_react/src/App.jsx
@@ -3,13 +3,11 @@ import './App.css'
 import {csv, csvFormat} from 'd3';
 
 const csvUrl = "https://gist.githubusercontent.com/SPMcDonald1316/c44c8efd90878b3c4032c3b642669e19/raw/cssNamedColors.csv";
-const width = 960;
-const height = 500;
 
 const message = data => {
   let message = '';
   message += `${Math.round(csvFormat(data).length / 1024)} kB\n`;
-  console.log(Math.round(csvFormat(data)));
+  console.log('hello');
   message += `${data.length} rows\n`;
   message += `${data.columns.length} columns\n`;
   return message;
@@ -23,7 +21,7 @@ const App = () => {
   });
 
   return (
-    <div>Data is {data ? message(data) : 'loading'}</div>
+    <pre>Data is {data ? message(data) : 'loading'}</pre>
   );
 };
 
