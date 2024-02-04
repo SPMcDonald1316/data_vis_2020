383c18c3e7c8124c04ddbd3439faa64a584c49cd
sean <spmcdonald1316@gmail.com>
2024-02-04 09:57:24 -0600

    restart

--- iterating_fast_vla/src/getData.js
+++
@@ -1,8 +0,0 @@
-import { csv } from 'd3';
-
-const csvUrl = 'https://gist.githubusercontent.com/curran/9938078a93a4ba380a0e/raw/8c489c0c1855c00f702f27c7546037e2941ae549/auto-mpg.csv'
-
-export const getData = async () => {
-  const data = await csv(csvUrl);
-  return data;
-}
