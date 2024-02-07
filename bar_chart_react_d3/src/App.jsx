b25ad46f1f76a6460f3e3a6e7b5c8c11b5b93e4f
sean <spmcdonald1316@gmail.com>
2024-02-07 16:49:04 -0600

    create bar chart showing population of all countries

--- bar_chart_react_d3/src/App.jsx
+++ bar_chart_react_d3/src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import { csv, scaleBand, scaleLinear, max } from 'd3';
+
+const csvUrl = 'https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv';
+const width = window.innerWidth;
+const height = window.innerHeight;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const row = d => {
+      d.Population = +d['2020'];
+      return d;
+    }
+    csv(csvUrl, row).then(setData);
+  }, []);
+
+  if(!data) {
+    return <div>Loading...</div>
+  }
+
+  const yScale = scaleBand()
+    .domain(data.map(d => d.Country))
+    .range([0, height]);
+
+  const xScale = scaleLinear()
+    .domain([0, max(data, d => d.Population)])
+    .range([0, width]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <svg width={width} height={height}>
+      {data.map(d => (
+        <rect 
+          x={0} 
+          y={yScale} 
+          width={xScale(d.Population)} 
+          height={yScale.bandwidth()}
+        />))}
+    </svg>
   )
 }
 
