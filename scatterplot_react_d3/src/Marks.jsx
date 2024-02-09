7b89ac749baa1bb80a4e2a5940a5503fa212d725
sean <spmcdonald1316@gmail.com>
2024-02-09 16:01:23 -0600

    adjust Marks component for scatterplot

--- scatterplot_react_d3/src/Marks.jsx
+++ scatterplot_react_d3/src/Marks.jsx
@@ -1,13 +1,12 @@
 export const Marks = ({data, xScale, yScale, xValue, yValue, tooltipFormat}) =>
-  data.map(d => (
-    <rect
-      key={yValue(d)}
+  data.map((d, index) => (
+    <circle
+      key={index}
       className="mark"
-      x={0}
-      y={yScale(yValue(d))}
-      width={xScale(xValue(d))}
-      height={yScale.bandwith()}
+      cx={xScale(xValue(d))}
+      cy={yScale(yValue(d))}
+      r={10}
     >
       <title>{tooltipFormat(xValue(d))}</title>
-    </rect>
+    </circle>
   ));
