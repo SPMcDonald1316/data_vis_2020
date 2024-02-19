ae86cc80d6db45f5952143148e1a8ad0b693c9e5
sean <spmcdonald1316@gmail.com>
2024-02-19 09:19:36 -0600

    fetch world atlas topojosn data and convert to geojson data

---
+++ world_map/src/useData.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+import { json } from "d3";
+import { feature } from "topojson";
+
+const jsonUrl = 'https://unpkg.com/world-atlas@2.0.2/countries-50m.json';
+
+export const useData = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    json(jsonUrl).then(topojsonData => {
+      const { countries } = topojsonData.objects;
+      setData(feature(topojsonData, countries));
+    });
+  }, [])
+
+  return data;
+}
