27fb9452d32b91bbfa95d1a0626189c4562097fc
sean <spmcdonald1316@gmail.com>
2024-02-19 09:20:11 -0600

    begin basic app structure

--- world_map/src/App.jsx
+++ world_map/src/App.jsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useData } from "./useData";
 
-function App() {
-  const [count, setCount] = useState(0)
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const App = () => {
+  const data = useData();
+
+  if (!data) return <pre>Loading...</pre>;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <svg width={width} height={height}>
+      <Marks data={data} />
+    </svg>
   )
 }
 
