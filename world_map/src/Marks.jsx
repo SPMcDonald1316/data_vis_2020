c69d40791d49e20eb0c3a4a9c6e59db63aa862d6
sean <spmcdonald1316@gmail.com>
2024-02-19 10:08:56 -0600

    add final styling to map

--- world_map/src/Marks.jsx
+++ world_map/src/Marks.jsx
@@ -1,13 +1,16 @@
-import { geoEqualEarth, geoPath } from "d3";
+import { geoNaturalEarth1, geoPath, geoGraticule } from "d3";
 
-const projection = geoEqualEarth();
+const projection = geoNaturalEarth1();
 const path = geoPath(projection);
+const graticule = geoGraticule();
 
-export const Marks = ({ data }) => (
+export const Marks = ({ data: { land, interiors } }) => (
   <g className="marks">
     <path className="sphere" d={path({ type: 'Sphere' })}></path>
-    {data.features.map(feature => (
-        <path className="feature" key={feature.id} d={path(feature)}></path>
+    <path className="graticules" d={path(graticule())}></path>
+    {land.features.map(feature => (
+        <path className="land" key={feature.id} d={path(feature)}></path>
     ))}
+    <path className="interior" d={path(interiors)}></path>
   </g>
 )
--- world_map/src/useData.js
+++ world_map/src/useData.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { json } from "d3";
-import { feature } from "topojson";
+import { feature,  mesh } from "topojson";
 
 const jsonUrl = 'https://unpkg.com/world-atlas@2.0.2/countries-50m.json';
 
@@ -8,9 +8,12 @@ export const useData = () => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    json(jsonUrl).then(topojsonData => {
-      const { countries } = topojsonData.objects;
-      setData(feature(topojsonData, countries));
+    json(jsonUrl).then(topology => {
+      const { countries, land } = topology.objects;
+      setData({
+        land: feature(topology, land),
+        interiors: mesh(topology, countries, (a, b) => a !== b )
+      });
     });
   }, [])
 
