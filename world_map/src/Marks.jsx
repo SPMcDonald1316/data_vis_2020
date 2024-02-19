14082de7f4a97aa304976a6e3a79b1f47e130817
sean <spmcdonald1316@gmail.com>
2024-02-19 09:45:59 -0600

    add background sphere to map and style

--- world_map/src/Marks.jsx
+++ world_map/src/Marks.jsx
@@ -5,8 +5,9 @@ const path = geoPath(projection);
 
 export const Marks = ({ data }) => (
   <g className="marks">
+    <path className="sphere" d={path({ type: 'Sphere' })}></path>
     {data.features.map(feature => (
-        <path key={feature.id} d={path(feature)}></path>
+        <path className="feature" key={feature.id} d={path(feature)}></path>
     ))}
   </g>
 )
