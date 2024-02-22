c64bf6772272d2a61241bb5f3fccb5bd9faf4638
sean <spmcdonald1316@gmail.com>
2024-02-22 15:50:10 -0600

    clean out all unneeded code

--- menus_in_react/src/App.jsx
+++ menus_in_react/src/App.jsx
@@ -1,16 +1,6 @@
-import { useData } from "./useData"
-
-const width = window.innerWidth;
-const height = window.innerHeight;
 
 const App = () => {
-  const data = useData();
-
-  if(!data) return <pre>Loading...</pre>
-
-  return (
-    <svg width={width} height={height}></svg>
-  )
+  <div>Hello React!</div>
 }
 
 export default App
