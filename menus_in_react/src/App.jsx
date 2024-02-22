e2da91b82ab18f6a8a0a6ec5925087780252e558
sean <spmcdonald1316@gmail.com>
2024-02-22 17:00:29 -0600

    move menu options to component, create dynamic menu using props

--- menus_in_react/src/App.jsx
+++ menus_in_react/src/App.jsx
@@ -1,19 +1,29 @@
-import './App.css'
+import './App.css';
+
+const Dropdown = ({options, id}) => (
+  <select id={id}>
+      <option value="">--Please choose an option--</option>
+      { options.map(({value, label}, index) => (
+        <option key={index} value={value}>{label}</option>
+      ))}
+    </select>
+);
+
+const options = [
+  {value: 'dog', label: 'Dog'},
+  {value: 'cat', label: 'Cat'},
+  {value: 'hamster', label: 'Hamster'},
+  {value: 'parrot', label: 'Parrot'},
+  {value: 'spider', label: 'Spider'},
+  {value: 'goldfish', label: 'Goldfish'}
+];
 
 const App = () => (
   <div>
     <label htmlFor="pet-select">Choose a pet: </label>
 
-    <select id="pet-select">
-      <option value="">--Please choose an option--</option>
-      <option value="dog">Dog</option>
-      <option value="cat">Cat</option>
-      <option value="hamster">Hamster</option>
-      <option value="parrot">Parrot</option>
-      <option value="spider">Spider</option>
-      <option value="goldfish">Goldfish</option>
-    </select>
+    <Dropdown options={options} id="pet-select" />
   </div>
-)
+);
 
 export default App
