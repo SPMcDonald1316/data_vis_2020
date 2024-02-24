dbbc0c53cd9821f235093ae902185cb31f8a99a9
sean <spmcdonald1316@gmail.com>
2024-02-24 10:24:25 -0600

    create scatterplot marks

---
+++ scatterplot_with_menus/src/Marks.jsx
@@ -0,0 +1,14 @@
+const Marks = ({ data, xScale, yScale, xValue, yValue, tooltipFormat, circleRadius}) =>
+  data.map((d, index) => (
+    <circle
+      key={index}
+      className="mark"
+      cx={xScale(xValue(d))}
+      cy={yScale(yValue(d))}
+      r={circleRadius}
+    >
+      <title>{tooltipFormat(xValue(d))}</title>
+    </circle>
+  ));
+
+export default Marks
