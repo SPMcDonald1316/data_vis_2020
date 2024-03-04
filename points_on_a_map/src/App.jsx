53bec2298382cdf48926fb5ec999fd5111234548
sean <spmcdonald1316@gmail.com>
2024-03-04 16:51:47 -0600

    create module to fetch/parse city data, import to App

--- points_on_a_map/src/App.jsx
+++ points_on_a_map/src/App.jsx
@@ -1,12 +1,14 @@
 import useWorldAtlas from "./useWorldAtlas.js";
 import Marks from "./Marks.jsx";
+import useCities from "./useCities.js";
 
 const width = window.innerWidth;
 const height = window.innerHeight;
 
 const App = () => {
   const mapData = useWorldAtlas();
-
+  const cityData = useCities();
+  console.log(cityData);
   if (!mapData) return <pre>Loading...</pre>;
 
   return (
