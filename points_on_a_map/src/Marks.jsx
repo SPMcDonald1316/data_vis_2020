6f1765c534a30df2755926339a8d2f9842dacc97
sean <spmcdonald1316@gmail.com>
2024-03-04 16:09:59 -0600

    write Marks component

---
+++ points_on_a_map/src/Marks.jsx
@@ -0,0 +1,18 @@
+import { geoNaturalEarth1, geoPath, geoGraticule } from "d3";
+
+const projection = geoNaturalEarth1();
+const path = geoPath(projection);
+const graticule = geoGraticule();
+
+const Marks = ({ data: {land, interiors }}) => (
+  <g className="marks">
+    <path className="sphere" d={path({ type: 'Sphere' })}></path>
+    <path className="graticules" d={path(graticule())}></path>
+    {land.features.map(feature => (
+      <path className="land" key={feature.id} d={path(feature)}></path>
+    ))}
+    <path className="interior" d={path(interiors)}></path>
+  </g>
+)
+
+export default Marks;
