f160aa3d1bae52d7d86e2e6ec0bcc82faaa4b377
sean <spmcdonald1316@gmail.com>
2024-03-04 17:06:50 -0600

    add cities to map, change styles

--- points_on_a_map/src/Marks.jsx
+++ points_on_a_map/src/Marks.jsx
@@ -4,7 +4,7 @@ const projection = geoNaturalEarth1();
 const path = geoPath(projection);
 const graticule = geoGraticule();
 
-const Marks = ({ worldAtlas: {land, interiors }, cities: { lat, lng}}) => (
+const Marks = ({ worldAtlas: {land, interiors }, cities}) => (
   <g className="marks">
     <path className="sphere" d={path({ type: 'Sphere' })}></path>
     <path className="graticules" d={path(graticule())}></path>
@@ -12,6 +12,10 @@ const Marks = ({ worldAtlas: {land, interiors }, cities: { lat, lng}}) => (
       <path className="land" key={feature.id} d={path(feature)}></path>
     ))}
     <path className="interior" d={path(interiors)}></path>
+    { cities.map((d, i) => {
+      const [x, y] = projection([d.lng, d.lat]);
+      return <circle key={i} cx={x} cy={y} r={1.5} />
+    })}
   </g>
 )
 
