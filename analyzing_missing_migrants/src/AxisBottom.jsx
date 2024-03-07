fb803b38cc635a3630cf37c0ac82766bdd656689
sean <spmcdonald1316@gmail.com>
2024-03-07 17:11:11 -0600

    begin AxisBottom component

---
+++ analyzing_missing_migrants/src/AxisBottom.jsx
@@ -0,0 +1,11 @@
+const AxisBottom = ({ xScale, innerHeight, tickFormat, tickOffset }) => 
+  xScale.ticks().map(tickValue => (
+    <g className="tick" key={tickValue} transform={`translate(${xScale(tickValue)}, 0)`}>
+      <line y2={innerHeight} />
+      <text style={{textAnchor: "middle"}} y={innerHeight + tickOffset} dy=".71em">
+        {tickFormat(tickValue)}
+      </text>
+    </g>
+  ));
+
+export default AxisBottom;
