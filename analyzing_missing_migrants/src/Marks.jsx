5a231529c472315ce03cddd92d42eeded0de07a8
sean <spmcdonald1316@gmail.com>
2024-03-08 09:15:55 -0600

    begin Marks component

---
+++ analyzing_missing_migrants/src/Marks.jsx
@@ -0,0 +1,11 @@
+import { curveNatural, line } from "d3";
+
+const Marks = ({data, xScale, yScale, xValue, yValue, tooltipFormat, circleRadius}) => 
+  <g className="marks">
+    <path
+      d={line()
+        .x(d => xScale(xValue(d)))
+        .y(d => yScale(yValue(d)))
+        .curve(curveNatural)(data)
+      } />
+  </g>
