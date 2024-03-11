f8f25352fff0510dfc44e1ecdc1f8bd56e9429d7
sean <spmcdonald1316@gmail.com>
2024-03-11 17:15:02 -0500

    change marks to use individual points instead of line

--- analyzing_missing_migrants/src/Marks.jsx
+++ analyzing_missing_migrants/src/Marks.jsx
@@ -2,12 +2,16 @@ import { curveNatural, line } from "d3";
 
 const Marks = ({data, xScale, yScale, xValue, yValue, tooltipFormat, circleRadius}) => 
   <g className="marks">
-    <path
-      d={line()
-        .x(d => xScale(xValue(d)))
-        .y(d => yScale(yValue(d)))
-        .curve(curveNatural)(data)
-      } />
+    {data.map((d, index) => (
+      <circle
+        key={index}
+        cx={xScale(xValue(d))}
+        cy={yScale(yValue(d))}
+        r={circleRadius}
+      >
+        <title>{tooltipFormat(xValue(d))}</title>
+      </circle>
+    ))}
   </g>
 
 export default Marks;
